53f655ff0dd5079202562658990d9f43a8f1088d
NandyArnold <143703535+NandyArnold@users.noreply.github.com>
2025-07-30 13:29:29 +0200

    Initial commit

---
+++ Assets/_OTSK/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+//__________________________________________________________
+
+// base FSM(finite state machine) for all enemy AI in the game
+
+//__________________________________________________________
+
+
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_OTSK/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+//_________________________________________________________
+
+
+
+// Base classs for all weapons in the game, maybe interface, TBD
+
+
+
+//___________________________________________________________
+public class Weapon : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
