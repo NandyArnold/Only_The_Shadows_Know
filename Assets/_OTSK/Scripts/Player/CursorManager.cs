7cf353eab2614bac67d55f5b701705b60d491fa9
NandyArnold <143703535+NandyArnold@users.noreply.github.com>
2025-08-06 17:11:54 +0200

    almost done fixing crosshair bug

--- Assets/_OTSK/Scripts/Player/CursorManager.cs
+++ Assets/_OTSK/Scripts/Player/CursorManager.cs
@@ -1,40 +1,59 @@
+using System;
 using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
     public static CursorManager Instance { get; private set; }
 
-    [Tooltip("The crosshair used for standard gameplay.")]
-    [SerializeField] private GameObject gameplayCrosshairUI;
+    [Tooltip("The crosshair PREFAB to be instantiated.")]
+    [SerializeField] private GameObject gameplayCrosshairPrefab;
 
+    private GameObject _crosshairInstance;
     private CursorState _currentState;
-    public event System.Action<CursorState> OnStateChanged;
+
+    public event Action<CursorState> OnStateChanged;
 
     private void Awake()
     {
-        if (Instance != null && Instance != this) Destroy(gameObject);
-        else Instance = this;
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
        
     }
 
-    // We now subscribe to events instead of using Start().
-    private void OnEnable()
+    private void Start()
     {
+        // Subscribe to events in Start() to ensure other managers are ready.
         if (GameManager.Instance != null)
         {
             GameManager.Instance.OnGameStateChanged += HandleGameStateChanged;
         }
+        PersistentObject.OnCanvasReady += InitializeCrosshair;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
+        // Always good practice to unsubscribe on destroy
         if (GameManager.Instance != null)
         {
             GameManager.Instance.OnGameStateChanged -= HandleGameStateChanged;
         }
+        PersistentObject.OnCanvasReady -= InitializeCrosshair;
     }
 
-    // This handler will be called automatically by the GameManager.
+    // This is called once by the PersistentObject when the canvas is ready.
+    private void InitializeCrosshair()
+    {
+        if (gameplayCrosshairPrefab != null && PersistentObject.Instance != null && _crosshairInstance == null)
+        {
+            _crosshairInstance = Instantiate(gameplayCrosshairPrefab, PersistentObject.Instance.transform);
+        }
+    }
+
+    // This is called by GameManager when the scene changes state.
     private void HandleGameStateChanged(GameState newState)
     {
         switch (newState)
@@ -43,13 +62,14 @@ public class CursorManager : MonoBehaviour
                 SetState(CursorState.Gameplay);
                 break;
             case GameState.Menu:
-            case GameState.Cutscene: // Also unlock cursor for cutscenes
-            case GameState.Loading:  // And for loading screens
+            case GameState.Cutscene:
+            case GameState.Loading:
                 SetState(CursorState.UI);
                 break;
         }
     }
 
+    // This is the single source of truth for setting the cursor's appearance.
     public void SetState(CursorState newState)
     {
         _currentState = newState;
@@ -58,19 +78,19 @@ public class CursorManager : MonoBehaviour
             case CursorState.Gameplay:
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
-                if (gameplayCrosshairUI != null) gameplayCrosshairUI.SetActive(true);
+                if (_crosshairInstance != null) _crosshairInstance.SetActive(true);
                 break;
 
             case CursorState.UI:
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
-                if (gameplayCrosshairUI != null) gameplayCrosshairUI.SetActive(false);
+                if (_crosshairInstance != null) _crosshairInstance.SetActive(false);
                 break;
 
             case CursorState.Targeting:
                 Cursor.lockState = CursorLockMode.Confined;
-                Cursor.visible = false;
-                if (gameplayCrosshairUI != null) gameplayCrosshairUI.SetActive(false);
+                Cursor.visible = false; // Cursor is hidden, player aims with decal
+                if (_crosshairInstance != null) _crosshairInstance.SetActive(false);
                 break;
         }
         OnStateChanged?.Invoke(_currentState);
--- Assets/_OTSK/Scripts/Utilities/PersistentObject.cs
+++ Assets/_OTSK/Scripts/Utilities/PersistentObject.cs
@@ -1,5 +1,25 @@
+// In PersistentObject.cs
+using System;
 using UnityEngine;
+
 public class PersistentObject : MonoBehaviour
 {
-    void Awake() => DontDestroyOnLoad(gameObject);
+    public static PersistentObject Instance { get; private set; }
+    
+    // An event to announce that the persistent canvas is ready
+    public static event Action OnCanvasReady;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        // Announce that this object is ready
+        OnCanvasReady?.Invoke();
+    }
 }
