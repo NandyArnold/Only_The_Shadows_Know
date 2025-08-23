a431e6040352df5a894427071b3da080647a8e5c
Nandy <143703535+NandyArnold@users.noreply.github.com>
2025-08-23 08:17:47 +0200

    All new new enemies have own Configs Setup

--- Assets/_OTSK/Scripts/EnvironmentAndObjects/RevealableType.cs
+++ Assets/_OTSK/Scripts/EnvironmentAndObjects/RevealableType.cs
@@ -2,8 +2,16 @@ public enum RevealableType
 {
     None,
     Normal,
+    Priority,
+    AlarmPanel,
+    Summoner,
+    EliteSummoner,
+    Instakill,
+    ManaAddition,
+    ArrowAddition,
     HiddenItem,
     Undead,
     Caster,
+    Elite,
     Boss
 }
